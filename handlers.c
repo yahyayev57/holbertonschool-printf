62469d518d2109b052ad4517f8a15f0346c4022d
Kenan <yahyayevkenan57@gmail.com>
2025-07-21 23:10:39 +0400

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * print_char - Prints a single character
+ * print_char - Prints a character
  * @args: Argument list
- *
- * Return: Number of characters printed (1)
+ * Return: Number of characters printed
  */
 int print_char(va_list args)
 {
@@ -17,7 +16,6 @@ int print_char(va_list args)
 /**
  * print_string - Prints a string
  * @args: Argument list
- *
  * Return: Number of characters printed
  */
 int print_string(va_list args)
@@ -38,9 +36,8 @@ int print_string(va_list args)
 }
 
 /**
- * print_percent_or_fallback - Handles %% or invalid format
+ * print_percent_or_fallback - Prints % or unknown specifier
  * @spec: Specifier character
- *
  * Return: Number of characters printed
  */
 int print_percent_or_fallback(char spec)
@@ -53,4 +50,48 @@ int print_percent_or_fallback(char spec)
 	write(1, "%", 1);
 	write(1, &spec, 1);
 	return (2);
+}
+
+/**
+ * print_int - Prints a signed integer
+ * @args: Argument list
+ * Return: Number of characters printed
+ */
+int print_int(va_list args)
+{
+	int num = va_arg(args, int);
+	int count = 0;
+	char buffer[12];
+	int i = 0;
+	unsigned int n;
+
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		count++;
+		n = -num;
+	}
+	else
+	{
+		n = num;
+	}
+
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		return (count + 1);
+	}
+
+	while (n > 0)
+	{
+		buffer[i++] = (n % 10) + '0';
+		n /= 10;
+	}
+
+	while (i--)
+	{
+		write(1, &buffer[i], 1);
+		count++;
+	}
+	return (count);
 }
