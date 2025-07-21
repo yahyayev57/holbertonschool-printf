706f7462652fd28b7dc025f275831cb6fb5adcf3
Kenan <yahyayevkenan57@gmail.com>
2025-07-21 23:12:14 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - Entry point
- *
  * Return: Always 0
  */
 int main(void)
 {
-	int len1, len2;
-
-	len1 = _printf("Hello %s!\n", "World");
-	len2 = printf("Hello %s!\n", "World");
-
-	_printf("Length: %d, Standard: %d\n", len1, len2);
-
-	_printf("Char: %c, Percent: %%\n", 'A');
-	_printf(NULL); /* should return -1 */
-
+	_printf("Signed int: %d\n", -1234);
+	_printf("Positive int: %i\n", 2025);
+	_printf("Zero: %d\n", 0);
 	return (0);
 }
