e0679c2634e6f31a4411be70bc2dcb5f4af90c10
Kenan <yahyayevkenan57@gmail.com>
2025-07-21 23:06:40 +0400

    Update main.h

--- main.h
+++ main.h
@@ -5,5 +5,8 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent_or_fallback(char spec);
 
 #endif /* MAIN_H */
